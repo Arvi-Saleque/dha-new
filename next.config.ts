19fe24bfa8d39657980cae7e3c3329e85dbd8664
Arvi_Saleque <alifsalek.as@gmail.com>
2025-10-04 09:56:39 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
     ],
-    formats: ['image/avif', 'image/webp'],
-    qualities: [75, 90],
+    formats: ["image/avif", "image/webp"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
 };
 
